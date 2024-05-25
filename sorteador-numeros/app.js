588098f493407eb25b0cc15922d566a834e94454
Gabriel Brito <dosgabrielbrito@gmail.com>
2024-05-25 12:24:33 -0300

    Aula 01

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,50 @@
+//Sorteio de Números:
+function sortear() {
+    let inputQuantidade = parseInt(document.getElementById('quantidade').value);
+    let inputDe = parseInt(document.getElementById('de').value);
+    let inputAte = parseInt(document.getElementById('ate').value);
+
+    let nomerosSorteados = [];
+    let numero;
+
+    for (let i = 0; i < inputQuantidade; i++) {
+        numero = obterNumeroAleatorio(inputDe, inputAte);
+
+        while (nomerosSorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(inputDe, inputAte);
+        }
+
+        nomerosSorteados.push(numero);
+    }
+
+    let labelResultado = document.getElementById('resultado');
+    labelResultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${nomerosSorteados}</label>`;
+    alterarStatusBotao('btn-reiniciar');
+}
+
+//Gerador de Número Aleatório:
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Alterar status do botão:
+function alterarStatusBotao(id) {
+    let botao = document.getElementById(id);
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+//Reiniciar Campos:
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+    alterarStatusBotao('btn-reiniciar');
+}
